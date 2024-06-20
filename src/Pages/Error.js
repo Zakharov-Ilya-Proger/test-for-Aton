b5bc271542cba8313207e73dc7f34092718a8954
Iiya Zakharov <2146376@gmail.com>
2024-06-20 15:45:35 +0300

    finish

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -7,7 +7,7 @@ function Error(){
             <h1 className="error-title">Ops!</h1>
             <p className="error-message">Что-то пошло не так!</p>
             <Link to="/" className="error-link">
-                <button className="error-button">Домой</button>
+                <button className="error-button">Login</button>
             </Link>
         </div>
     );
